e2447e0f0e6aeed51a59426d401ffe3cc027321a
Teogenes <teogenes.santos@gmail.com>
2020-04-06 20:21:10 -0300

    Declaracao do servico PaypalService

---
+++ src/model/services/PaypalService.java
@@ -0,0 +1,19 @@
+package model.services;
+
+import mobel.interfaces.OnlinePaymentService;
+
+public class PaypalService implements OnlinePaymentService{
+
+	@Override
+	public Double paymentFee(Double amount) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Double interest(Double amount, Integer months) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
