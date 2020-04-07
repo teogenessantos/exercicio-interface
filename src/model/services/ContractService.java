4513ad7a7feb1d532c30a9c3a04c8b1e1e16dfed
Teogenes <teogenes.santos@gmail.com>
2020-04-07 10:11:42 -0300

    Inclusao do construtor do servico ContractService

--- src/model/services/ContractService.java
+++ src/model/services/ContractService.java
@@ -1,5 +1,17 @@
 package model.services;
 
-public class ContractService {
+import mobel.interfaces.OnlinePaymentService;
+import model.entities.Contract;
 
+public class ContractService {
+	private OnlinePaymentService onlinePaymentService;
+	
+	
+	public ContractService(OnlinePaymentService onlinePaymentService) {
+		this.onlinePaymentService = onlinePaymentService;
+	}
+
+	public void processContract(Contract contract, Integer months) {
+		
+	}
 }
